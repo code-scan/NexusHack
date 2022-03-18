3514288c51895d8070dd0ed4466619a954dfb922
cond0r@manjaro <manjaro@aq.mk>
2022-03-19 04:56:57 +0800

    修改readme

--- main.go
+++ main.go
@@ -12,12 +12,11 @@ func main() {
 	flag.StringVar(&host, "host", "", "1.1.1.1:8080")
 	flag.StringVar(&registry, "registry", "", "xx-registry")
 	flag.IntVar(&thread, "thread", 20, "20")
-
 	flag.Parse()
 	if host != "" && registry != "" {
 		d := docker.NewDocker(host, thread, registry)
 		d.GetImages()
-		//d.GetBlobs(registry)
+		d.GetBlobs(registry)
 		d.ExtractFsLayer(registry)
 	}
 }
